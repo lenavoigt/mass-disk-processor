74990125864a22f13df16e8aec924d394689cbaa
Lena Voigt <lena.lucia.voigt@fau.de>
2025-07-25 16:58:39 +0200

    refactor disk_size plugin: revise total_sector method

--- mdp_plugins/disk_size.py
+++ mdp_plugins/disk_size.py
@@ -8,25 +8,20 @@ class DiskSize(object):
     description = 'Gets total size of the disk image (as raw)'
     include_in_data_table = True
 
-    def get_disk_size(self, disk_image):
+    @staticmethod
+    def get_disk_size(disk_image):
         disk_size = disk_image.get_media_size()
 
         return disk_size
 
-    def get_total_sectors(self, disk_image):
+    @staticmethod
+    def get_total_sectors(disk_image):
         # This is derived from tsk partition information at the moment
-        # TODO: There will be a better implementation. Check TSK underlying properties
-        # might be property of pytsk3.Img_Info
-        total_sectors = None
         parts = disk_image._get_partitions()
-        last = None
-        for each in parts:
-            last = each
-
-        if last is not None:
-            total_sectors = last.start + last.len
-
-        return total_sectors
+        if not parts:
+            return None
+        max_sector = max((part.start + part.len for part in parts), default=None)
+        return max_sector
 
 
     def process_disk(self, target_disk_image: TargetDiskImage):
