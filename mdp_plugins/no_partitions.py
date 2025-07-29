e771cbda50a6369d92c7476a278b436e4994649e
Lena Voigt <lena.lucia.voigt@fau.de>
2025-07-29 15:44:52 +0200

    refactor no_partitions plugin: add error_handling to partition_count

--- mdp_plugins/no_partitions.py
+++ mdp_plugins/no_partitions.py
@@ -10,8 +10,12 @@ class NumberOfPartitions(object):
 
     def process_disk(self, target_disk_image: TargetDiskImage):
         disk_image = target_disk_image.accessor
-        partitions = disk_image.partitions
-        no_partitions = len(partitions)
+        try:
+            partitions = disk_image.partitions
+            no_partitions = len(partitions)
+        except Exception as e:
+            print(e)
+            no_partitions = None
         # print('no_partitions: {}'.format(no_partitions))
 
         res = mdp_lib.plugin_result.MDPResult(target_disk_image.image_path, self.name, self.description)
