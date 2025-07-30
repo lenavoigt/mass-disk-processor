160338b0b7c374b142bc2537595f04da83a1f31b
Lena Voigt <lena.lucia.voigt@fau.de>
2025-07-30 22:33:39 +0200

    add func win_evt_logins: add count for windows startup (4608) and user initiated logoff (4647)

--- mdp_plugins/win_evt_logins.py
+++ mdp_plugins/win_evt_logins.py
@@ -15,25 +15,33 @@ class EVTXLogs(object):
     include_in_data_table = True
 
     def process_disk(self, target_disk_image: TargetDiskImage):
+        # sources:
+        # https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/plan/appendix-l--events-to-monitor
+        # https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4624
+        # https://www.alteredsecurity.com/post/fantastic-windows-logon-types-and-where-to-find-credentials-in-them#viewer-5movr
+
         disk_image = target_disk_image.accessor
         files = disk_image.files
 
         temp_filename = 'export.bin'
 
+        start_ups = None
         succ_login = None
         failed_login = None
         unlocks = None
         clock_change = None
+        log_offs = None
 
         for each_file in files:
             if re.match('.*/winevt/Logs/Security.evtx$', each_file.full_path, re.IGNORECASE):
                 # print(each_file.full_path)
 
+                start_ups = 0
                 succ_login = 0
                 failed_login = 0
                 unlocks = 0
                 clock_change = 0
-
+                log_offs = 0
 
                 f = open(temp_filename, 'wb')
                 f.write(each_file.read())
@@ -43,7 +51,9 @@ class EVTXLogs(object):
                     for record in log.records():
                         data_dict = xmltodict.parse(record.xml())
                         event_id = int(data_dict['Event']['System']['EventID']['#text'])
-                        if event_id == 4624:    # successful login
+                        if event_id == 4608:    # Windows startup event
+                            start_ups +=1
+                        elif event_id == 4624:    # successful login
                             for each in data_dict['Event']['EventData']['Data']:  # for this one we need to loop through to find the LogonType (2 is interactive logon)
                                 if each.get('@Name') == 'LogonType':
                                     if each.get('#text') == '2':  # this is interactive logon
@@ -54,20 +64,21 @@ class EVTXLogs(object):
                             failed_login += 1
                         elif event_id == 4616:     # clock change (vista onwards https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4616)
                             clock_change += 1
+                        elif event_id == 4647:     # user initiated logoff events
+                            log_offs += 1
                 break  # stop if Security evtx is processed
 
-        # some event log info :
-        #    https://www.alteredsecurity.com/post/fantastic-windows-logon-types-and-where-to-find-credentials-in-them#viewer-5movr
-        #    https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4624
 
         if os.path.exists(temp_filename):
             os.remove(temp_filename)
 
         res = mdp_lib.plugin_result.MDPResult(target_disk_image.image_path, self.name, self.description)
-        res.results = {'evtx_success_logins_4624_2': succ_login,
-                       'evtx_failed_logins_4625': failed_login,
-                       'evtx_unlocks_4624_7': unlocks,
-                       'evtx_clock_change_4616': clock_change
+        res.results = {'evtx_win_startup_4608': start_ups,
+                        'evtx_success_logins_4624_2': succ_login,
+                        'evtx_failed_logins_4625': failed_login,
+                        'evtx_unlocks_4624_7': unlocks,
+                        'evtx_clock_change_4616': clock_change,
+                        'evtx_user_initiated_logoff_4647': log_offs
                        }
         return res
 
