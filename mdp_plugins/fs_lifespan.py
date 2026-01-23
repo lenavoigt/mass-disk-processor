9ef9deddb7b2b075cc2212909262e6e47e2b9186
Chris Hargreaves <chrishargreaves@users.noreply.github.com>
2026-01-23 20:51:11 +0100

    Patches filesystem lifespan plugin to not crash with iOS backups

--- mdp_plugins/fs_lifespan.py
+++ mdp_plugins/fs_lifespan.py
@@ -18,26 +18,32 @@ class FSLifespan(MDPPlugin):
         disk_image = target_disk_image.accessor
         files = disk_image.files
 
-        sorted_list = sorted(files, key=lambda d: d.timestamps['cr_time'])
+        try:
+            sorted_list = sorted(files, key=lambda d: d.timestamps['cr_time'])
 
-        earliest = sorted_list[0].timestamps['cr_time']
-        earliest_str = datetime.datetime.fromtimestamp(earliest).isoformat()
-        # print('earliest: {} ({}) {}'.format(sorted_list[0].full_path, earliest, earliest_str))
-        latest = sorted_list[-1].timestamps['cr_time']
-        latest_str = datetime.datetime.fromtimestamp(latest).isoformat()
-        # print('latest: {} ({}) {}'.format(sorted_list[-1].full_path, latest, latest_str))
+            earliest = sorted_list[0].timestamps['cr_time']
+            earliest_str = datetime.datetime.fromtimestamp(earliest).isoformat()
+            # print('earliest: {} ({}) {}'.format(sorted_list[0].full_path, earliest, earliest_str))
+            latest = sorted_list[-1].timestamps['cr_time']
+            latest_str = datetime.datetime.fromtimestamp(latest).isoformat()
+            # print('latest: {} ({}) {}'.format(sorted_list[-1].full_path, latest, latest_str))
 
-        diff = latest - earliest  # in seconds
-        diff_datetime = datetime.datetime.fromtimestamp(latest) - datetime.datetime.fromtimestamp(earliest)
-        diff_str = str(diff_datetime)
-        # print('diff: {} ({})'.format(diff, diff_str))
+            diff = latest - earliest  # in seconds
+            diff_datetime = datetime.datetime.fromtimestamp(latest) - datetime.datetime.fromtimestamp(earliest)
+            diff_str = str(diff_datetime)
+            # print('diff: {} ({})'.format(diff, diff_str))
 
+        except:
+            diff = 0
+            diff_str = "NA"
+            earliest_str = "NA"
+            latest_str = "NA"
+        
         result = self.create_result(target_disk_image)
         self.set_results(result, {
-            'lifespan_fs_cr': diff,
-            'lifespan_fs_cr_str': diff_str,
-            'earliest_fs_cr': earliest_str,
-            'latest_fs_cr': latest_str
-        })
-
+                'lifespan_fs_cr': diff,
+                'lifespan_fs_cr_str': diff_str,
+                'earliest_fs_cr': earliest_str,
+                'latest_fs_cr': latest_str
+            })
         return result
